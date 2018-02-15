1285144e5e33bace56abc5951df08c46d6463c20
nothingButSocksOn <adrian.sj@hotmail.com>
2018-02-15 16:15:46 +0100

    la till variabler lengde- og bredde-grader og radius pluss getters og setters på disse

--- app/src/main/java/com/example/schnappi/inschool/Variables.java
+++ app/src/main/java/com/example/schnappi/inschool/Variables.java
@@ -5,4 +5,32 @@ package com.example.schnappi.inschool;
  */
 
 public class Variables {
+
+    double breddegrad;
+    double lengdegrade;
+    int radius;
+
+    public double getBreddegrad() {
+        return breddegrad;
+    }
+
+    public double getLengdegrade() {
+        return lengdegrade;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setBreddegrad(double breddegrad) {
+        this.breddegrad = breddegrad;
+    }
+
+    public void setLengdegrade(double lengdegrade) {
+        this.lengdegrade = lengdegrade;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 }
